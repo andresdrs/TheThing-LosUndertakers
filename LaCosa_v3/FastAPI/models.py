abb8227c5f502ffc39eebaacd57961a782eaed5f
Juan Bratti <100317321+juanbratti@users.noreply.github.com>
2023-10-02 02:41:56 -0300

    actualizado modelos
    ahora los games no tienen un número de id, el id es el nombre del juego
    
    agregado atributo players_in que es la cantidad de jugadores adentro

--- LaCosa_v3/FastAPI/models.py
+++ LaCosa_v3/FastAPI/models.py
@@ -11,14 +11,14 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     nickname = Column(String(50), unique = True ) # largo es 50
     is_creator = Column(Boolean)
-    user_game = Column(Integer) 
+    user_game = Column(String(50)) 
 
 class Game(Base):
     __tablename__ = 'games'
 
-    id = Column(Integer, primary_key=True, index=True)
-    game_name = Column(String(50), unique=False)
-    game_state = (Integer) # podemos definir a los estados como enteros (0 no inciada, 1 iniciada, 2 finalizada)
+    game_name = Column(String(50), primary_key=True) # no tenemos id de game, el id será el nombre. No puede haber dos partidas con el mismo nombre
+    game_state = Column(Integer) # podemos definir a los estados como enteros (0 no inciada, 1 iniciada, 2 finalizada)
+    players_in = Column(Integer)
     min_players = Column(Integer)
     max_players = Column(Integer)
-    players_ingame = Column(Integer)
+    id_creator = Column(Integer)
